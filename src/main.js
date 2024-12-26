44a7cf9e7caaf5a27df3d863a80e76687e8f6ed5
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-12-27 02:31:00 +0300

    innerHTML example

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@
 document.querySelector("#app").innerHTML =
   '<h1 style="text-align: center;">Hello</h1>';
 
-//alert(); -> Basic Page Pop-up Box
-alert("This Is Pop-Up Box");
+// alert(); -> Basic Page Pop-up Box
+//alert("This Is Pop-Up Box");
 
-// console.log(); -> Logs a Message To The Console
+// console.log(); -> Logs A Message To The Console
 console.log("Page Loaded!");
+
+// innerHTML -> Gets Or Sets The HTML Content Of An Element
+document.getElementById("paragraph").innerHTML = "First Paragraph Text";
