a162e0b90f09813317781bfbf815e014bd0ad6a3
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-12-27 02:18:16 +0300

    alert function.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-document.querySelector("#app").innerHTML = "Hello";
+//Welcome Text
+document.querySelector("#app").innerHTML =
+  '<h1 style="text-align: center;">Hello</h1>';
+
+//alert(); -> Basic Page Pop-up Box
+alert("This Is Pop-Up Box");
