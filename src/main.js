308deacbffb9f5313b6b5c7cd8513c8d92db934f
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-12-27 16:41:04 +0300

    debugger example

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ document.querySelector("#app").innerHTML =
   '<h1 style="text-align: center;">Hello</h1>';
 
 // alert(); -> Basic Page Pop-up Box
-//alert("This Is Pop-Up Box");
+alert("This Is Pop-Up Box");
 
 // console.log(); -> Logs A Message To The Console
 console.log("Page Loaded!");
 
 // innerHTML -> Gets Or Sets The HTML Content Of An Element
 document.getElementById("paragraph").innerHTML = "First Paragraph Text";
+
+// debugger; -> Used for debugging.
+debugger;
