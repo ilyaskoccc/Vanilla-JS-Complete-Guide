7342d64ae7483dcaf250dc19a810be6de9cdd5e1
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-12-31 17:38:14 +0300

    Update exampleArraySort.js

--- src/array/exampleArraySort.js
+++ src/array/exampleArraySort.js
@@ -1,5 +1,5 @@
 export function exampleArraySort() {
-  // Sort
+  // Sort Number
   const numbers = [500, 200, 50, 256, 45];
   const sortNum = numbers.sort((a, b) => {
     return a - b;
@@ -11,11 +11,52 @@ export function exampleArraySort() {
   // Appending data type results to the div
   printDiv.innerHTML = `
                       <br/>
-                      <h2>-Sort</h2> 
-                      <p>Numbers → ${numbers} </p>  
+                      <h2>-Sort Number</h2> 
                       <p>Sort → ${sortNum} </p>  
                       `;
 
   // Appending the div to the body
   document.body.appendChild(printDiv);
+
+  // Sort String
+  function sortStr(a, b) {
+    const alphabet =
+      "AaBbCcÇçDdEeFfGgĞğHhIıİiJjKkLlMmNnOoÖöPpQqRrSsŞşTtUuÜüVvWwXxYyZz";
+
+    if (a.lenght === 0 || b.lenght === 0) {
+      return a.lenght - b.lenght;
+    } else {
+      let one = alphabet.indexOf(a[0]);
+      let two = alphabet.indexOf(b[0]);
+
+      if (one !== two) {
+        return one - two;
+      } else {
+        return sortStr(a.slice(1), b.slice(1));
+      }
+    }
+  }
+  const names = [
+    "Volkan",
+    "Ümit",
+    "Onur",
+    "Serkan",
+    "Hakan",
+    "Çağatay",
+    "Şinasi",
+  ];
+  const sortNames = names.sort(sortStr);
+
+  // Creating a div to append all the data type information
+  const printDiv1 = document.createElement("div");
+
+  // Appending data type results to the div
+  printDiv1.innerHTML = `
+                        <br/>
+                        <h2>-Sort String</h2> 
+                        <p>Sort → ${sortNames} </p>  
+                        `;
+
+  // Appending the div to the body
+  document.body.appendChild(printDiv1);
 }
